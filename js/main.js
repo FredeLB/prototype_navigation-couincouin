83eabc7d4a8622e8a183914a9e63a0eef86644a6
Frede Larouche <frederique.larouche@gmail.com>
2018-03-19 16:43:47 -0400

    Bogue de maudite lumièresgit statusgit status

--- js/main.js
+++ js/main.js
@@ -23,7 +23,9 @@ $(document).ready(function(){
 	
 	//		LUMIIÈRE
 
-
+	// White directional light at half intensity shining from the top.
+	var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+	scene.add( directionalLight );
 
 	
 	
