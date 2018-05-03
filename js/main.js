80cb9403e644697323c6903d46c7f4bcedf90f48
Frede Larouche <frederique.larouche@gmail.com>
2018-05-03 17:52:20 -0400

    Corrections touch

--- js/main.js
+++ js/main.js
@@ -290,8 +290,9 @@ $(document).ready(function(){
     
     function onTap(event){
         
-        mouse.x = ( event.touches[0].pageX / window.innerWidth ) * 2 - 1;
-        mouse.y = - ( event.touches[0].pageX / window.innerHeight ) * 2 + 1;
+        mouse.x = +(event.targetTouches[0].pageX / window.innerwidth) * 2 +-1;
+
+        mouse.y = -(event.targetTouches[0].pageY / window.innerHeight) * 2 + 1;
         
     }
 	
@@ -620,8 +621,9 @@ $(document).ready(function(){
 		
 	window.addEventListener( 'resize', onWindowResize, false );
 	document.addEventListener("click", funcDansLaZone, false);
-	document.addEventListener("touchend", funcDansLaZone, false);
-	document.addEventListener( 'mousemove', onTap, false );
+	//document.addEventListener("touchstart", funcDansLaZone, false);
+	document.addEventListener( 'mousemove', onMouseMove, false );
+	document.addEventListener( 'touchend', onTap, false );
 	
 	$("#btncouin").on('click touchend', function(e){
 		
